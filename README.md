c749b1723d27146f0d82b0cb8af51e5632c512f7
Robin Hillyard <r.hillyard@northeastern.edu>
2023-08-08 20:19:24 -0400

    Unknown changes 2

---
+++ README.md
@@ -0,0 +1,5 @@
+AirBNB
+======
+
+This is simply a main program which creates a table using TableParser and then uses
+Spark to process the rows.
--- src/main/scala/AirBNB.scala
+++ src/main/scala/AirBNB.scala
@@ -1,18 +1,16 @@
-import com.phasmidsoftware.parse.{RawTableParser, TableParser}
-import com.phasmidsoftware.table.{HeadedTable, RawTable, Table}
+import com.phasmidsoftware.parse.RawTableParser
+import com.phasmidsoftware.table.{HeadedTable, RawTable}
 import com.phasmidsoftware.util.FP
-import com.phasmidsoftware.util.FP.resource
 import org.apache.spark.rdd.RDD
-
-import scala.util._
-import org.apache.spark.sql.{Dataset, SparkSession}
+import org.apache.spark.sql.SparkSession
 
 import scala.Console.println
 import scala.io.Source
+import scala.util._
 
 object AirBNB extends App {
 
-//    val airBNBFile = "/AIRBNB.Listing.csv"
+  //    val airBNBFile = "/AIRBNB.Listing.csv"
   val airBNBFile = "/airbnb2.csv"
   // Set up the source
   val sy: Try[Source] = for (u <- FP.resourceForClass(airBNBFile)) yield Source.fromURL(u)
@@ -20,20 +18,20 @@ object AirBNB extends App {
   val parser = parser1.setMultiline(true)
 
   val wsty: Try[RawTable] = parser parse sy
-//    Table.parseResourceRaw(airBNBFile)
-    wsty match {
-      case Success(HeadedTable(r, h)) =>
-        println(s"AirBNB: successfully read ${r.size} rows")
-        println(s"AirBNB: successfully read ${h.size} columns")
+  //    Table.parseResourceRaw(airBNBFile)
+  wsty match {
+    case Success(HeadedTable(r, h)) =>
+      println(s"AirBNB: successfully read ${r.size} rows")
+      println(s"AirBNB: successfully read ${h.size} columns")
 
-        val session = SparkSession.builder()
-          .master("local")
-          .appName("AirBNB")
-          .getOrCreate()
+      val session = SparkSession.builder()
+        .master("local")
+        .appName("AirBNB")
+        .getOrCreate()
 
-        val rdd: RDD[Seq[String]] = session.sparkContext.makeRDD(r.toStream)
-        rdd.map(ws => ws.mkString(",")).foreach(println)
+      val rdd: RDD[Seq[String]] = session.sparkContext.makeRDD(r.toStream)
+      rdd.map(ws => ws.mkString(",")).foreach(println)
 
-      case _ =>
+    case _ =>
     }
 }
